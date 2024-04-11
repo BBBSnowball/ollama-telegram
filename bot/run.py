92442c17b7a32ae15fca0ff9a00c3ed5986debe9
Benjamin Koch <bbbsnowball@gmail.com>
2024-04-11 02:25:28 +0200

    don't sent partial reply if it is very short
    We wait for certain characters but they can be near the start of the
    reply: "Hello! ...", "Yes, ..."

--- bot/run.py
+++ bot/run.py
@@ -254,7 +254,7 @@ async def ollama_request(message: types.Message):
             if full_response_stripped == "":
                 continue
 
-            if "." in chunk or "\n" in chunk or "!" in chunk or "?" in chunk or "," in chunk:
+            if ("." in chunk or "\n" in chunk or "!" in chunk or "?" in chunk or "," in chunk) and len(full_response_stripped) >= 30:
                 if message.from_user.id in no_stream_ids:
                     # user prefers no streaming
                     pass
